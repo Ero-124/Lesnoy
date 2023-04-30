a8cbd043f7bd2f69569e387d5308db2758fa0251
Ero-124 <khachatryanero.2002@mail.ru>
2023-04-30 18:21:51 +0400

    Worked for responsiveness

--- assets/js/home.js
+++ assets/js/home.js
@@ -4,17 +4,19 @@ $(document).ready(function () {
     $("#top-sales__slider").owlCarousel({
         loop: true,
         margin: 20,
-        lazyLoad:true,
+        lazyLoad: true,
         autoplay: true,
         autoplayTimeout: 2500,
         autoplayHoverPause: true,
         nav: false,
         dots: false,
+        responsiveClass: true,
         responsive: {
-            0: {
+            360: {
                 items: 1,
+                margin: 16
             },
-            600: {
+            479: {
                 items: 2,
             },
             1000: {
@@ -22,19 +24,68 @@ $(document).ready(function () {
             },
             1368: {
                 items: 4,
+                margin: 20,
             }
         }
     });
 
+
+    $("#main-slider").owlCarousel({
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 3500,
+        autoplayHoverPause: true,
+        nav: false,
+        dots: false,
+        slideSpeed: 1000,
+        items: 1,
+        singleItems: true,
+        animateOut: 'fadeOut'
+    });
+
 })
 
-/* Rotating img */
-const itemContainer = document.querySelector('.sort__wrapper');
-const rotateImg = document.querySelector('.sort__img');
 
-itemContainer.addEventListener("mouseover", (e) => {
-    const rotation = e.target.getAttribute("data-rotation");
-    if (rotation) {
-        rotateImg.style.transform = `rotate(${rotation}deg)`;
+
+/* Rotating img */
+function addRotationEffect() {
+    const itemContainer = document.querySelector('.sort__wrapper');
+    const rotateImg = document.querySelector('.sort__img');
+
+    if (window.innerWidth >= 1024) {
+        itemContainer.addEventListener("mouseover", (e) => {
+            const rotation = e.target.getAttribute("data-rotation");
+            if (rotation) {
+                rotateImg.style.transform = `rotate(${rotation}deg)`;
+            }
+        });
     }
-});
+}
+
+addRotationEffect();
+
+
+
+
+/* function handleWindowResize() {
+    const headerNav = document.querySelector('.header__nav');
+    const headerContacts = document.querySelector('.header__contacts');
+    const headerBasket = document.querySelector('.header__basket');
+    const headerLogo = document.querySelector('.header__logo');
+
+    if (window.innerWidth <= 991) {
+        if (headerNav) headerNav.remove();
+        if (headerContacts) headerContacts.remove();
+        if (headerBasket) headerBasket.remove();
+    } else {
+        if (headerNav && !headerLogo.nextElementSibling.classList.contains('header__nav')) {
+            headerLogo.after(headerNav);
+        }
+    }
+}
+
+// Добавляем обработчик события изменения размера окна
+window.addEventListener('resize', handleWindowResize);
+
+// Вызываем функцию handleWindowResize() при загрузке страницы
+handleWindowResize(); */
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,164 @@
-// Dropdown navigation
-const dropdowns = document.querySelectorAll(".dropdown");
+/* Dropdown navigation */
+const dropdowns = document.querySelector('.navbar__list');
 
-dropdowns.forEach((dropdown) => {
-  const dropdownList = dropdown.querySelector(".dropdown__list");
-  dropdown.addEventListener("click", (e) => {
-    if (e.target.classList.contains("dropdown") || e.target.tagName === "I") {
-      dropdownList.classList.toggle("active");
-    }
-  });
-});
+dropdowns.addEventListener('click', (event) => {
+  const target = event.target;
+  const dropdown = target.closest('.dropdown');
 
-// Dropdown close
-document.addEventListener("click", (e) => {
-  dropdowns.forEach((dropdown) => {
+  if (dropdown) {
     const dropdownList = dropdown.querySelector(".dropdown__list");
-    if (!dropdown.contains(e.target)) {
-      dropdownList.classList.remove("active");
+
+    if (target.classList.contains("dropdown") || target.tagName === "I") {
+      dropdownList.classList.toggle("active");
+      dropdown.classList.toggle('active');
+    }
+
+    if (window.innerWidth <= 1024 && dropdownList.classList.contains('active')) {
+      dropdown.style.marginBottom = dropdownList.clientHeight + 'px';
+    } else {
+      dropdown.style.marginBottom = 0;
+    }
+  }
+});
+
+/* Dropdown close and burger menu */
+const burger = document.querySelector(".header__burger");
+const navbar = document.querySelector('.navbar__list');
+
+document.addEventListener('click', (event) => {
+  const target = event.target;
+
+  // Close dropdowns
+  const dropdownLists = document.querySelectorAll('.dropdown__list');
+  dropdownLists.forEach((dropdownList) => {
+    const dropdown = dropdownList.closest('.dropdown');
+    if (dropdown && !dropdown.contains(target)) {
+      dropdownList.classList.remove('active');
+      dropdown.classList.remove('active');
+      dropdown.style.marginBottom = 0;
     }
   });
+
+  // Close burger menu
+  if (!target.closest('.header__burger') && !target.closest('.navbar__list') && navbar.classList.contains('active')) {
+    burger.classList.remove('active');
+    navbar.classList.remove('active');
+    document.body.classList.remove('lock');
+  }
+});
+
+/* Toggle lock class on body */
+function toggleLockBody() {
+  document.body.classList.toggle('lock');
+}
+
+// Burger menu click event
+burger.addEventListener('click', () => {
+  burger.classList.toggle('active');
+  navbar.classList.toggle('active');
+  toggleLockBody();
 });
 
 
+/* Input visible or hidden */
+const searchIcon = document.getElementById('search-icon');
+const searchInput = document.querySelector('#header .header__search input');
+
+searchIcon.addEventListener('click', function () {
+  searchInput.classList.add('visible');
+});
+
+
+
+/* Adding menu items */
+/* <li><a href="">О компании</a></li> */
+
+// function addFooterItems() {
+//   /* Footer Items */
+//   const footerLeft = document.querySelector('#footer .footer__left ');
+//   const createDivElement = document.createElement('div')
+//   createDivElement.classList.add('navigation')
+
+//   const createAElement = document.createElement('a');
+//   const createAElement2 = document.createElement('a');
+
+//   createAElement.textContent = 'О компании';
+//   createAElement2.textContent = 'Доставка и услуги';
+
+//   createDivElement.insertAdjacentElement("afterbegin", createAElement2)
+//   createDivElement.insertAdjacentElement('afterbegin', createAElement)
+
+//   footerLeft.insertAdjacentElement("beforeend", createDivElement)
+
+
+// }
+
+// addFooterItems();
+
+
+
+let menuItemsAdded = false;
+
+function addMenuItems() {
+  if (menuItemsAdded) {
+    return; 
+  }
+
+  /* Header Items */
+
+  const navbarList = document.querySelector('#navbar .navbar__list');
+  const createLiElement = document.createElement('li');
+  const createLiElement2 = document.createElement('li');
+  createLiElement.classList.add('navbar__item--mobile');
+  createLiElement2.classList.add('navbar__item--mobile');
+
+  const createAElement = document.createElement('a');
+  createAElement.setAttribute('href', "#")
+  const createAElement2 = document.createElement('a');
+  createAElement2.setAttribute('href', "#")
+  createAElement.textContent = 'О компании';
+  createAElement2.textContent = 'Доставка и услуги';
+  createAElement.classList.add('navbar__link');
+  createAElement2.classList.add('navbar__link');
+
+  createLiElement.insertAdjacentElement('afterbegin', createAElement);
+  createLiElement2.insertAdjacentElement('afterbegin', createAElement2);
+  navbarList.insertAdjacentElement('afterbegin', createLiElement);
+  createLiElement.insertAdjacentElement('afterend', createLiElement2);
+
+  menuItemsAdded = true; 
+}
+
+function removeMenuItems() {
+  const navbarList = document.querySelector('#navbar .navbar__list');
+  const navbarItems = navbarList.querySelectorAll('.navbar__item--mobile');
+  navbarItems.forEach(item => item.remove());
+}
+
+
+function handleResize() {
+  if (window.innerWidth <= 1024.99) {
+    addMenuItems();
+    menuItemsAdded = true;
+    // removeHeaderNav(true);
+  } else {
+    removeMenuItems();
+    menuItemsAdded = false;
+    // removeHeaderNav(false);
+  }
+}
+
+/* function removeHeaderNav(isDelete) {
+  const headerLogo = document.querySelector('.header__logo');
+  const headerNav = document.querySelector('.header__nav');
+  let headerNavCopy;
+  if (isDelete) {
+    headerNav.remove();
+  } else {
+    headerNavCopy = headerNav;
+    headerLogo.insertAdjacentElement("afterend", headerNavCopy);
+  }
+} */
+
+window.addEventListener('resize', handleResize);
+handleResize();
